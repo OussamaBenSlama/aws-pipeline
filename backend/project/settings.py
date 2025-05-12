94f89e2b1aa6b6285a90ee85ba0c15565f40a89c
oussama <benslamaoussama1919@gmail.com>
2025-05-12 22:44:11 +0100

    allow all hosts in django

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -9,16 +9,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4f6morbgs82onehr(3$!n+c$6s=y@v-hf)&^nj)-4v#e4z#o$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
